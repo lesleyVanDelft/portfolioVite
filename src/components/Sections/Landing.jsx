c883c681d535cd3bc43f1b9a638a8d0e0e945038
Lesley van Delft <lesleyvandelft92@gmail.com>
2022-12-09 17:41:58 +0100

    start adding framer motion

--- src/components/Sections/Landing.jsx
+++ src/components/Sections/Landing.jsx
@@ -1,7 +1,25 @@
 import blobSvg from '../../assets/img/blobanimation.svg';
+import { motion } from 'framer-motion';
+
+const variants = {
+	hidden: {
+		opacity: 0,
+	},
+	show: {
+		opacity: 1,
+	},
+	transition: {
+		duration: 600,
+	},
+};
+
 const Landing = () => {
 	return (
-		<section className="Landing">
+		<motion.section
+			className="Landing"
+			animate={{ opacity: 1 }}
+			hidden={{ opacity: 0 }}
+			transition={{ duration: 3 }}>
 			<div className="Landing__content">
 				<h3 className="Landing__content--welcome">Hi there!</h3>
 				<h2 className="Landing__content--name">My name is Lesley.</h2>
@@ -15,7 +33,7 @@ const Landing = () => {
 					About me &#8595;
 				</button>
 			</div>
-		</section>
+		</motion.section>
 	);
 };
 
