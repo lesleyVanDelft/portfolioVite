d908c0c7871dd80425e393106fac49ff2cf2104e
Lesley van Delft <lesleyvandelft92@gmail.com>
2022-12-18 14:48:58 +0100

    projectitem styling

--- src/components/Sections/ProjectsItem.jsx
+++ src/components/Sections/ProjectsItem.jsx
@@ -2,10 +2,15 @@ const ProjectsItem = ({ project, index }) => {
 	return (
 		<article className="Projects__item">
 			<h4 className="Projects__item--title">{project.title}</h4>
+			<div className="Projects__item--technologies">
+				{project.tech?.map((el, i) => {
+					return <p key={i}>{el}</p>;
+				})}
+			</div>
 			<figure className="Projects__item--image">
 				<img src={project.image} alt="" />
-				<p className="Projects__item--description">{project.description}</p>
 			</figure>
+			<p className="Projects__item--description">{project.description}</p>
 		</article>
 	);
 };
--- src/data.js
+++ src/data.js
@@ -3,6 +3,7 @@ import imageDesigno from './assets/img/officialThumbnail2.png';
 export const projectList = [
 	{
 		title: 'Designo',
+		tech: ['React', 'Sass'],
 		image: imageDesigno,
 		description:
 			'A simple static site using React. This was my first time implementing Google Maps into a website. ',
