15c9ea89dd47fc9efd24889d000a327608ca6979
Lesley van Delft <lesleyvandelft92@gmail.com>
2023-01-18 06:08:02 +0100

    netlify host test

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Landing from './components/Sections/Landing';
 import Navbar from './components/Navbar/Navbar';
 import Projects from './components/Sections/Projects';
 import Test from './pages/Test';
-import './dist/css/index.css';
+import './styles/css/index.css';
 import About from './components/Sections/About';
 import Container from './components/Container';
 
