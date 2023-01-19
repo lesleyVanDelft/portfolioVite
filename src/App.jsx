2e23ccca2028d057e6dfda1585f22b316dd32c9c
Lesley van Delft <lesleyvandelft92@gmail.com>
2023-01-19 20:32:51 +0100

    cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -4,32 +4,6 @@ import Home from './pages/Home';
 import Tester from './pages/Tester';
 
 function App() {
-	const [loading, setLoading] = useState(true);
-
-	// const asyncLoading = () => {
-	// 	return new Promise(resolve => setTimeout(() => resolve(), 2500));
-	// };
-
-	useEffect(() => {
-		const onPageLoad = () => {
-			setLoading(false);
-		};
-		// console.log(document.readyState);
-
-		// document.readyState === 'complete' ? setLoading(false) :
-		// if (document.readyState === 'interactive') {
-		// 	setLoading(true);
-		// 	console.log('readyState interact');
-		// } else {
-		// 	window.addEventListener('load', setLoading(false));
-		// 	console.log('loaded');
-		// }
-		// window.addEventListener('load', onPageLoad());
-		setTimeout(() => {
-			setLoading(false);
-		}, 1500);
-	}, []);
-
 	return (
 		<div className="App">
 			<Home />
--- src/main.jsx
+++ src/main.jsx
@@ -4,22 +4,6 @@ import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
-		<form name="contact" method="post" netlify="true" hidden>
-			<input type="hidden" name="form-name" value="contact" />
-			<p>
-				<label>
-					Your Name: <input type="text" name="name" />
-				</label>
-			</p>
-			<p>
-				<label>
-					Message: <textarea name="message"></textarea>
-				</label>
-			</p>
-			<p>
-				<button type="submit">Send</button>
-			</p>
-		</form>
 		<App />
 	</React.StrictMode>
 );
