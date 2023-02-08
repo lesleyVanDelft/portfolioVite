edbc2b43aab13142a200d45add8ce72d8e86184d
Lesley van Delft <lesleyvandelft92@gmail.com>
2023-02-08 19:34:58 +0100

    add project to data.js

--- src/components/Sections/ProjectsItem.jsx
+++ src/components/Sections/ProjectsItem.jsx
@@ -4,7 +4,11 @@ const ProjectsItem = ({ project, index }) => {
 			<h4 className="Projects__item--title">{project.title}</h4>
 			<div className="Projects__item--technologies">
 				{project.tech?.map((el, i) => {
-					return <p key={i}>{el}</p>;
+					return (
+						<p key={i} className={`tech`}>
+							{el}
+						</p>
+					);
 				})}
 			</div>
 			<figure className="Projects__item--image">
